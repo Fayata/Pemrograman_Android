2b52ab642d0bbaaf9a8d7d5a629190a536bd38ea
Daffa Surya Septa <113518282+Fayata@users.noreply.github.com>
2023-09-19 08:40:56 +0700

    1

---
+++ Konsep Intent/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+    }
+    public void onClick(View view){
+        Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+        startActivity(moveIntent);
+    }
+}
