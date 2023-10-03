d058e0261157316f30cb52987c88704bbc0693da
Daffa Surya Septa <113518282+Fayata@users.noreply.github.com>
2023-10-03 07:42:40 +0700

    Intent

--- Konsep Intent/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Konsep Intent/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.myapplication;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    private TextView tvResult;
+    final ActivityResultLauncher<Intent>resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result ->  {
+        if (result.getResultCode() == MoveForResultActivity.RESULT_CODE&&result.getData() !=null){
+            int selectValue= result.getData().getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE,0);
+            tvResult.setText(String.format("Hasil : %s", selectValue));
+        }
+    });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +27,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btnMoveActivity = findViewById(R.id.btn_move_activity);
         btnMoveActivity.setOnClickListener(this);
-        
+
         Button btnMoveWithDataActivity = findViewById(R.id.btn_move_activity_data);
         btnMoveWithDataActivity.setOnClickListener(this);
+
+        Button btnDialPhone = findViewById(R.id.btn_dial_number);
+        btnDialPhone.setOnClickListener(this);
+
+        Button btnMoveForResult = findViewById(R.id.btn_move_for_result);
+        btnMoveForResult.setOnClickListener(this);
+
+        tvResult = findViewById(R.id.tv_result);
+
+
     }
-    public void onClick(View view){
-        if (view.getId() == R.id.btn_move_activity){
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_move_activity) {
             Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
             startActivity(moveIntent);
         } else if (view.getId() == R.id.btn_move_activity_data) {
@@ -29,6 +50,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_KULIAH, "PEMROGRAMAN BERBASIS MOBILE");
             moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_KELAS, 5);
             startActivity(moveWithDataIntent);
+        } else if (view.getId() == R.id.btn_dial_number) {
+            String phoneNumber = "089617991010";
+            Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+            startActivity(dialPhoneIntent);
+        } else if (view.getId() == R.id.btn_move_for_result) {
+            Intent moveForResultIntent = new Intent(MainActivity.this, MoveForResultActivity.class);
+            resultLauncher.launch(moveForResultIntent);
         }
+
     }
 }
